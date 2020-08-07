221849ce3155ed7ab3ffd5d58fb2691580f00f35
ryandinulfatah <ryandfbasketball12@gmail.com>
2020-08-07 20:08:33 +0700

    add hamburger menu, fix responsive, and other

--- build/main.js
+++ build/main.js
@@ -150,3 +150,10 @@ module.exports = __webpack_require__(/*! D:\Programmer\Code\HTML_CSS\Voler\src\a
 /***/ })
 
 /******/ });
+
+
+const menuToggle = document.querySelector('.menu-toggle input');
+const nav = document.querySelector('nav');
+menuToggle.addEventListener('click', function () {
+  nav.classList.toggle('active');
+});
